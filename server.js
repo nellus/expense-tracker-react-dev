ede88c39e99838ea1f9dcb7e7ff932d71fc82bfe
simone.martinello <s.martinello@lutech.it>
2020-06-17 16:42:07 +0200

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/api/customers', (req, res) => {
   const customers = [
